7341763595bba603bb2a8c92bcb9732b983b053c
johannes112 <johannes.emmos@gmail.com>
2015-05-20 17:52:43 +0000

    headless: add gemfile,delete old empty folder,edit env.rb

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'selenium-webdriver'
+gem 'headless'
+gem 'cucumber'
+gem 'rails'
+gem "json"
+gem "capybara"
+gem "rspec"
---
+++ features/support/env.rb
@@ -0,0 +1,17 @@
+
+if ENV['HEADLESS'] == 'true'
+  $stdout.write 'Headless\n'
+  require 'headless'
+  require 'capybara/cucumber'
+      Capybara.default_driver = :selenium
+  headless = Headless.new
+  headless.start
+
+  at_exit do
+    headless.destroy
+  end
+else
+    $stdout.write 'With\n'
+    require 'capybara/cucumber'
+    Capybara.default_driver = :selenium
+end
