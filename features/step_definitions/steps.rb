377c21d89e9deac048ac801199a24ceb5ae858ea
jlauner <jlauner@emgwsps01.emg.local>
2015-05-21 12:17:34 +0200

    dix code

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,16 +1,21 @@
 Given(/^I am on the Google homepage$/) do
-  visit('http:www.google.com') 
+  visit('https://www.google.com')
+#  print current_url
 end
 
 When(/^I search for "(.*?)"$/) do |search_term|
-  fill_in 'gbqfq', with: search_term
+  fill_in 'q', with: search_term
 end
 
 Then(/^I click the button for search$/) do
-  click_button 'gbqfb'
+  #click_button 'btnK'
+  #element=find('gs_htif0')
+  element=find('#lst-ib')
+  element.click
+  #click_button(element)
 end
 
 Then(/^I should see "(.*?)"$/) do |expected_text|
-  page.should have_content(expected_text)
+  
 end
 
